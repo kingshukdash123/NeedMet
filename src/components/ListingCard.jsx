e7bd8f49572bb9d7cba3f07b4ff359efbfc9ce94
Kingshuk Dash <kingshukdash123@gmail.com>
2026-03-28 21:48:31 +0530

    add overflow with '...' in listing card

--- src/components/ListingCard.jsx
+++ src/components/ListingCard.jsx
@@ -25,11 +25,11 @@ export default function ListingCard({listing}) {
       <div className="listing-content">
         <p className="listing-category">{listing.category}</p>
 
-        <h3 className="listing-title">{listing.name.length > 20? listing.name.slice(0, 20) + "...": listing.name}</h3>
+        <h3 className="listing-title">{listing.name}</h3>
 
         <div className="listing-location">
           <i className="fa-solid fa-location-dot loc-icon"></i>
-          <span>{listing.address.length > 23 ? listing.address.slice(0, 23) + "..." : listing.address}</span>
+          <span>{listing.address}</span>
         </div>
 
         <div className="listing-card-rating">
